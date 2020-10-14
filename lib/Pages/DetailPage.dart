582b7c23d9700f9f49058e54dd06b77fe216cb5f
hi.ahmad30 <p136015@nu.edu.pk>
2020-10-14 12:20:06 -0700

    listpage

--- lib/Pages/DetailPage.dart
+++ lib/Pages/DetailPage.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
-import 'package:minorityreport/Utils/Consts.dart';
 //import 'package:google_fonts/google_fonts.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
 import 'package:path_provider/path_provider.dart';
