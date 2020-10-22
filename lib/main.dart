5eea9830e9a9fadd01d54ebdc9f3c29672cf6755
Ahmad Javed <p136015@nu.edu.pk>
2020-10-22 20:09:00 +0500

    00

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:firebase_core/firebase_core.dart';
 
 import 'package:flutter/material.dart';
-
+import 'package:get/get.dart';
+import '';
 import 'Pages/DetailEnter.dart';
 import 'Pages/First_Page.dart';
 import 'Pages/ListPage.dart';
@@ -23,7 +24,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Minority Report',
       theme: ThemeData(
         primarySwatch: Colors.blue,
