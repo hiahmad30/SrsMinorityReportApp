48b74671d4b57c6cbe7e8e4f2e48422b2b99a918
hiahmad30 <p136015@nu.edu.pk>
2021-01-25 09:10:08 +0500

    list fix

--- lib/Pages/ListPage.dart
+++ lib/Pages/ListPage.dart
@@ -166,7 +166,7 @@ class _RatingListState extends State<RatingList> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text(
-                  document.get("BusinessName").toString(),
+                  document.get("BussinessName").toString(),
                   style: TextStyle(
                     color: Colors.white,
                     fontSize: 20,
@@ -218,7 +218,6 @@ class _RatingListState extends State<RatingList> {
               ],
             ),
           ),
-
           ButtonBar(
             alignment: MainAxisAlignment.start,
             children: [
@@ -239,7 +238,6 @@ class _RatingListState extends State<RatingList> {
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.01,
               ),
-              
             ],
           ),
         ],
